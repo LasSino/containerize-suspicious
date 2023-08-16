f42b7a737e99064eb0346dda579976515245ac63
LasSino <ltwsamuel@outlook.com>
2023-08-16 14:07:30 +0800

    Initial commit.

---
+++ Scripts/sandbox-setup.cmd
@@ -0,0 +1,3 @@
+powershell.exe -executionpolicy bypass -file C:\Scripts\addzh-cn.ps1
+
+C:\Program Files (x86)\Common Files\Tencent\QQProtect\Bin\QQProtect.exe
---
+++ prepare_containerize.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+MAPPED_FOLDER_TEMPLATE = File.open('./mapped_folder.xml.tmpl', 'rt', &:read)
+SANDBOX_TEMPLATE = File.open('./Containerize.wsb.tmpl', 'rt', &:read)
+MAPPING_FILE_DIRECTORY = './directory_mappings/'
+FINAL_SANDBOX_FILE = 'Containerize.wsb'
+
+require 'fileutils'
+
+def ensure_folder(path = '')
+  abs_path = File.expand_path(path, './')
+  FileUtils.mkdir_p(abs_path)
+  abs_path
+end
+
+def process_entry(sandbox, host, ro)
+  abs_path = ensure_folder(host)
+  mapping_xml = MAPPED_FOLDER_TEMPLATE.gsub('{{sandbox}}', sandbox)
+  mapping_xml.gsub!('{{host}}', abs_path)
+  mapping_xml.gsub!('{{ro}}', ro ? 'True' : 'False')
+  mapping_xml
+end
+
+require 'yaml'
+
+def process_mapping_file(filename)
+  mapping_list = []
+  mapping_obj = YAML.load_file(filename)
+  mapping_obj.each do |entry|
+    mapping_list << process_entry(entry['sandbox'], entry['host'], entry['ro'])
+  end
+  mapping_list
+end
+
+final_mapping_list = []
+
+Dir.each_child(MAPPING_FILE_DIRECTORY) do |fn|
+  full_fn = MAPPING_FILE_DIRECTORY + fn
+  final_mapping_list += process_mapping_file(full_fn) if File.file?(full_fn)
+end
+
+mapping_folder_xml = final_mapping_list.join("\n")
+final_xml = SANDBOX_TEMPLATE.gsub('{{OtherMappedFolders}}', mapping_folder_xml)
+File.open(FINAL_SANDBOX_FILE, 'wt') { |f| f.write(final_xml) }
+
